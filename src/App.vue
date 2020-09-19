6afe68058c1835f50c311ac6ca42b312b7dc6552
Hussain Aminu <hussain_aminu@yahoo.com>
2020-09-19 08:48:05 +0100

    few changes to file structure

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,44 @@
 <template>
-  <div>
-    <Todos todos_prop.sync="todos"></Todos>
+  <div class="container mx-16 my-6">
+    <h1 class="font-semibold">hello {{name}} and {{greet('afternoon')}}</h1>
+    <button
+      class="text-purple-500 hover:text-white hover:bg-purple-500 border border-purple-500 text-sm font-semibold rounded-full px-4 py-1 leading-normal"
+      @click="increase(10)"
+    >increase</button>
+    <p class="font-semibold">my age is: {{age}}</p>
+    <button
+      class="text-purple-500 hover:text-white hover:bg-purple-500 border border-purple-500 text-sm font-semibold rounded-full px-4 py-1 leading-normal"
+      @click="decrease(10)"
+    >decrease</button>
+    <a class="block" :href="website">my awesome site o</a>
+    <input
+      type="text"
+      :value="name"
+      class="border border-purple-400 rounded-lg pl-2 focus:border-none"
+    />
   </div>
 </template>
 
 <script>
-import Todos from "@/components/Todos.vue";
 export default {
   name: "App",
-  components: {
-    Todos,
-  },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "first todo",
-          completed: false,
-        },
-        {
-          id: 2,
-          title: "second todo",
-          completed: true,
-        },
-        {
-          id: 3,
-          title: "third todo",
-          completed: false,
-        },
-      ],
+      name: "Hussain",
+      age: 27,
+      website: "https://twitter.com",
     };
+  },
+  methods: {
+    greet(time) {
+      return `good ${time} sir`;
+    },
+    increase(increment) {
+      this.age += increment;
+    },
+    decrease(decrement) {
+      this.age -= decrement;
+    },
   },
 };
 </script>

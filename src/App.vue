0183db250360cba650da0e4fcf668f9a03fd86e9
Hussain Aminu <hussain_aminu@yahoo.com>
2020-09-22 15:12:57 +0100

    upgraded to stable release of vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
-  <div class="container mx-16 my-6">
-    <h1 class="font-semibold">hello {{name}} and {{greet('afternoon')}}</h1>
+  <div id="app" class="container mx-16 my-6">
+    <h1 class="font-semibold">hello {{name}} and {{greet('day')}}</h1>
     <button
       class="text-purple-500 hover:text-white hover:bg-purple-500 border border-purple-500 text-sm font-semibold rounded-full px-4 py-1 leading-normal"
-      @click="increase(10)"
+      @click="increase(1)"
     >increase</button>
-    <p class="font-semibold">my age is: {{age}}</p>
     <button
       class="text-purple-500 hover:text-white hover:bg-purple-500 border border-purple-500 text-sm font-semibold rounded-full px-4 py-1 leading-normal"
-      @click="decrease(10)"
+      @dblclick="increase(10)"
+    >increase2x</button>
+    <p class="font-semibold">age is: {{age}}</p>
+    <button
+      class="text-purple-500 hover:text-white hover:bg-purple-500 border border-purple-500 text-sm font-semibold rounded-full px-4 py-1 leading-normal"
+      @click="decrease(1)"
     >decrease</button>
-    <a class="block" :href="website">my awesome site o</a>
+    <button
+      class="text-purple-500 hover:text-white hover:bg-purple-500 border border-purple-500 text-sm font-semibold rounded-full px-4 py-1 leading-normal"
+      @dblclick="decrease(10)"
+    >decrease2x</button>
+    <a class="block" :href="website">my awesome site</a>
     <input
       type="text"
       :value="name"
       class="border border-purple-400 rounded-lg pl-2 focus:border-none"
     />
+  </div>
+  <div class>
+    <p id="canvas" class="border rounded-md border-black ml-6" @mousemove="updateXY">{{x}}, {{y}}</p>
   </div>
 </template>
 
@@ -27,6 +38,8 @@ export default {
       name: "Hussain",
       age: 27,
       website: "https://twitter.com",
+      x: 0,
+      y: 0,
     };
   },
   methods: {
@@ -34,12 +47,27 @@ export default {
       return `good ${time} sir`;
     },
     increase(increment) {
-      this.age += increment;
+      setTimeout(() => {
+        this.age += increment;
+      }, 1000);
     },
     decrease(decrement) {
       this.age -= decrement;
+    },
+    updateXY(event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     },
   },
 };
 </script>
 
+
+
+<style lang="css" scoped>
+#canvas {
+  width: 600px;
+  height: 300px;
+  text-align: center;
+}
+</style>
